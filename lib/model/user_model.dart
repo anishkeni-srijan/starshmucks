d0438419f99af44a32a7f199c2efd6efa0f42f34
Sanish Aukhale <sanish.aukhale@srijan.net>
2022-10-18 20:24:11 +0530

    DB MODEL

---
+++ lib/model/user_model.dart
@@ -0,0 +1,27 @@
+import 'package:hive/hive.dart';
+
+part 'user_model.g.dart';
+
+@HiveType(typeId: 0)
+class UserData extends HiveObject {
+  @HiveField(0)
+  late String name;
+
+  @HiveField(1)
+  late String email;
+
+  @HiveField(2)
+  late String phone;
+
+  @HiveField(3)
+  late String dob;
+
+  @HiveField(4)
+  late String password;
+
+  @HiveField(4)
+  late bool tnc = false;
+
+  @HiveField(5)
+  late bool isactive = false;
+}
