1006bf31abcddd7bb3bc9e6e21418217486daae2
Sanish Aukhale <sanish.aukhale@srijan.net>
2022-11-03 11:36:25 +0530

    cartDB

---
+++ lib/model/cart_model.dart
@@ -0,0 +1,18 @@
+import 'package:hive/hive.dart';
+
+part 'cart_model.g.dart';
+
+@HiveType(typeId: 0)
+class UserData extends HiveObject {
+  @HiveField(0)
+  late String title;
+
+  @HiveField(1)
+  late String price;
+
+  @HiveField(2)
+  late String qty;
+
+  @HiveField(3)
+  late bool isInCart = false;
+}
