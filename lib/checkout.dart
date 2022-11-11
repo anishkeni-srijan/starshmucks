60a823ee6cf95a448c8422730101ded7efa0f87a
anish.keni <anish.keni@srijan.net>
2022-11-11 15:38:55 +0530

    multiple addresses no db

--- lib/checkout.dart
+++ lib/checkout.dart
@@ -12,14 +12,9 @@ import 'boxes.dart';
 import 'model/cart_model.dart';
 import 'model/user_model.dart';
 
-final fname = TextEditingController();
-final phone = TextEditingController();
-final hno = TextEditingController();
-final roadname = TextEditingController();
-final city = TextEditingController();
-final state = TextEditingController();
-final pincode = TextEditingController();
-late final List address = [];
+late List<Map<String,dynamic>>location = [];
+
+
 
 class Checkout extends StatefulWidget {
   const Checkout({Key? key}) : super(key: key);
@@ -44,7 +39,16 @@ class _CheckoutState extends State<Checkout> {
     super.initState();
   }
 
+
+
   addAddress(context) {
+    final fname = TextEditingController();
+    final phone = TextEditingController();
+    final hno = TextEditingController();
+    final roadname = TextEditingController();
+    final city = TextEditingController();
+    final state = TextEditingController();
+    final pincode = TextEditingController();
     final box = Boxes.getUserData();
     final data = box.values.toList().cast<UserData>();
 
@@ -314,15 +318,22 @@ class _CheckoutState extends State<Checkout> {
                   width: 300,
                   child: ElevatedButton(
                     onPressed: () {
-                      address.add(fname.text);
-                      address.add(phone.text);
-                      address.add(hno.text);
-                      address.add(roadname.text);
-                      address.add(city.text);
-                      address.add(state.text);
-                      address.add(pincode.text);
-                      data[userkey].address = address;
-                      box.put(userkey, data[userkey]);
+
+
+                      // data[userkey].address.add(result);
+                      // box.put(userkey, data[userkey]);
+
+                      var xresult = {
+                        'name':fname.text,
+                        'phno':phone.text,
+                        'hno':hno.text,
+                        'area':roadname.text,
+                        'city':city.text,
+                        'state':state.text,
+                        'pincode':pincode.text,
+                      };
+                      location.add(xresult);
+
                     },
                     style: ElevatedButton.styleFrom(
                       shape: RoundedRectangleBorder(
@@ -331,6 +342,318 @@ class _CheckoutState extends State<Checkout> {
                     ),
                     child: Text(
                       'SIGN UP',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  editAddress(context, index) {
+    final box = Boxes.getUserData();
+    final data = box.values.toList().cast<UserData>();
+    final fname = TextEditingController(text: location[index]['name']);
+    final phone = TextEditingController(text: location[index]['phno']);
+    final hno = TextEditingController(text: location[index]['hno']);
+    final roadname = TextEditingController(text: location[index]['area']);
+    final city = TextEditingController(text: location[index]['city']);
+    final state = TextEditingController(text: location[index]['state']);
+    final pincode = TextEditingController(text: location[index]['pincode']);
+
+    PhoneNumber number = PhoneNumber(isoCode: 'IN');
+    showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (BuildContext context) {
+        return SingleChildScrollView(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height * 0.70,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                    margin: EdgeInsets.all(20),
+                    alignment: Alignment.center,
+                    child: Text(
+                      "Edit Address",
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        color: HexColor("#175244"),
+                      ),
+                    )),
+
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.005,
+                  ),
+                  child: TextFormField(
+                    style: const TextStyle(color: Colors.black), //<-- SEE HERE
+                    controller: fname,
+
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(5),
+                      labelText: 'Full Name',
+                      labelStyle: TextStyle(
+                        color: HexColor("#175244"),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: HexColor("#175244"),
+                          width: 2,
+                        ),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: HexColor("#175244"),
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                //phone number
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  height: 80,
+                  child: InternationalPhoneNumberInput(
+                    selectorConfig: SelectorConfig(
+                        trailingSpace: false,
+                        selectorType: PhoneInputSelectorType.DROPDOWN),
+                    autoValidateMode: AutovalidateMode.disabled,
+                    selectorTextStyle: TextStyle(color: HexColor("#175244")),
+                    initialValue: number,
+                    textFieldController: phone,
+                    inputDecoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(5),
+                      labelText: 'Phone Number',
+                      labelStyle: TextStyle(
+                        color: HexColor("#175244"),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: HexColor("#175244"),
+                          width: 2,
+                        ),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: HexColor("#175244"),
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                    keyboardType: TextInputType.number,
+                    onInputChanged: (PhoneNumber value) {},
+                  ),
+                ),
+                //house no, building name
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.005,
+                  ),
+                  child: TextFormField(
+                    style: const TextStyle(color: Colors.black), //<-- SEE HERE
+                    controller: hno,
+
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(5),
+                      labelText: 'House No., Building Name',
+                      labelStyle: TextStyle(
+                        color: HexColor("#175244"),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: HexColor("#175244"),
+                          width: 2,
+                        ),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: HexColor("#175244"),
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                //road name,area,colony
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.005,
+                  ),
+                  child: TextFormField(
+                    style: const TextStyle(color: Colors.black), //<-- SEE HERE
+                    controller: roadname,
+
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(5),
+                      labelText: 'Road Name, Area, Colony',
+                      labelStyle: TextStyle(
+                        color: HexColor("#175244"),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: HexColor("#175244"),
+                          width: 2,
+                        ),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: HexColor("#175244"),
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                //city
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.005,
+                  ),
+                  child: TextFormField(
+                    style: const TextStyle(color: Colors.black), //<-- SEE HERE
+                    controller: city,
+
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(5),
+                      labelText: 'City',
+                      labelStyle: TextStyle(
+                        color: HexColor("#175244"),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: HexColor("#175244"),
+                          width: 2,
+                        ),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: HexColor("#175244"),
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                //state
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.005,
+                  ),
+                  child: TextFormField(
+                    style: const TextStyle(color: Colors.black), //<-- SEE HERE
+                    controller: state,
+
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(5),
+                      labelText: 'State',
+                      labelStyle: TextStyle(
+                        color: HexColor("#175244"),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: HexColor("#175244"),
+                          width: 2,
+                        ),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: HexColor("#175244"),
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                //pincode
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.8,
+                  margin: EdgeInsets.only(
+                    top: MediaQuery.of(context).size.height * 0.005,
+                  ),
+                  child: TextFormField(
+                    style: const TextStyle(color: Colors.black), //<-- SEE HERE
+                    controller: pincode,
+
+                    decoration: InputDecoration(
+                      contentPadding: EdgeInsets.all(5),
+                      labelText: 'Pincode',
+                      labelStyle: TextStyle(
+                        color: HexColor("#175244"),
+                      ),
+                      enabledBorder: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: HexColor("#175244"),
+                          width: 2,
+                        ),
+                      ),
+                      focusedBorder: UnderlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
+                        borderSide: BorderSide(
+                          color: HexColor("#175244"),
+                          width: 2,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                SizedBox(
+                  // height: 100,
+                  width: 300,
+                  child: ElevatedButton(
+                    onPressed: () {
+
+                      // data[userkey].address[index]=result;
+                      // box.put(userkey, data[userkey]);
+                      var xresult = {
+                        'name':fname.text,
+                        'phno':phone.text,
+                        'hno':hno.text,
+                        'area':roadname.text,
+                        'city':city.text,
+                        'state':state.text,
+                        'pincode':pincode.text,
+                      };
+                      location[index]=xresult;
+                    },
+                    style: ElevatedButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(60)),
+                      backgroundColor: HexColor("#036635"),
+                    ),
+                    child: Text(
+                      'Save',
                       style: TextStyle(
                         fontSize: 18,
                         color: Colors.white,
@@ -377,29 +700,55 @@ class _CheckoutState extends State<Checkout> {
               );
             } else {
               //int? len = int?.parse(data[0].address.length! / 7);
-              return ListView.builder(
-                itemCount: data[0].address.length,
-                itemBuilder: (context, index) {
-                  return Column(
-                    children: [
-                      Row(
-                        children: [
-                          Container(
-                            width: 350,
-                            child: Text(
-                              data[0].address[index],
+              return Column(
+                children: [
+                  Expanded(
+                    child: ListView.builder(
+                      itemCount: location.length,
+                      itemBuilder: (context, index) {
+                        return Column(
+                          children: [
+                            Text(location[index].toString()),
+                            // Row(
+                            //   children: [
+                            //     Container(
+                            //       width: 350,
+                            //       child: Text(
+                            //         data[0].address[index],
+                            //       ),
+                            //     ),
+                            //   ],
+                            // ),
+                            Container(
+                              child: Row(
+                                children: [
+                                  TextButton(
+                                      onPressed: () {
+                                        editAddress(context, index);
+                                      },
+                                      child: Text('Edit')),
+                                  TextButton(
+                                      onPressed: () {
+                                        data[0].address.removeAt(index);
+                                        location.removeAt(index);
+                                        setState(() {});
+                                      },
+                                      child: Text('Remove'))
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      TextButton(
-                          onPressed: () {
-                            addAddress(context);
-                          },
-                          child: Text('Add a new address')),
-                    ],
-                  );
-                },
+                          ],
+                        );
+                      },
+                    ),
+                  ),
+                  TextButton(
+                      onPressed: () {
+                        addAddress(context);
+                        print(data[0].address.toString());
+                      },
+                      child: Text('Add a new address')),
+                ],
               );
             }
           }),
