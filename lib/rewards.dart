376c0874906f8426dca0f306bb0d961444f03f7f
anish.keni <anish.keni@srijan.net>
2022-11-16 12:24:25 +0530

    rewards page

--- lib/rewards.dart
+++ lib/rewards.dart
@@ -19,148 +19,157 @@ class _RewardsState extends State<Rewards> {
     final box = Boxes.getUserData();
     final data = box.values.toList().cast<UserData>();
     return Scaffold(
-      appBar: AppBar(
-      title: Text('Rewards'),
-      backgroundColor: Colors.white,
-      foregroundColor: HexColor("#175244"),
-    ),
-      body: SingleChildScrollView(child: Column(
-        children: [
-      Container(
-      height: MediaQuery.of(context).size.height * 0.2,
-          decoration: BoxDecoration(
-            color: HexColor("#175244"),
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              colorFilter: ColorFilter.mode(
-                Colors.black.withOpacity(0.05),
-                BlendMode.dstATop,
-              ),
-              image: ExactAssetImage('images/shmucks.png'),
-            ),
-          ),
-          child: Column(
-            children: [
-              Container(
-                transform: Matrix4.translationValues(0, 28, 0),
-                child: Text(
-                  '${'Hi ' + username}!',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20,
-                  ),
-                ),
-              ),
-      Container(
-      padding: EdgeInsets.only(
-      bottom: 20,
-        left: 20,
-      ),
-          transform: Matrix4.translationValues(
-            0,
-            80,
-            0,
-          ),
-          child: Row(
-            children: [
-              Container(
-                transform: Matrix4.translationValues(
-                  3,
-                  -8,
-                  0,
-                ),
-                child: Image.asset(
-                  'images/stars.png',
-                  width: 20,
-                ),
-              ),
-              Column(
+        appBar: AppBar(
+          title: Text('Rewards'),
+          backgroundColor: Colors.white,
+          foregroundColor: HexColor("#175244"),
+        ),
+        body: SingleChildScrollView(
+            child: Column(
+
+          children: [
+            Container(
+              margin: EdgeInsets.only(top: 20, left: 20),
+              alignment: Alignment.topLeft,
+              child: Row(
                 children: [
-                  AutoSizeText(
-                    '1/5',
+                  Text(
+                    '${'Hi ' + username}',
                     style: TextStyle(
-                      color: Colors.white,
+                      color: HexColor("#175244"),
+                      fontSize: 25,
                     ),
-                    minFontSize: 12,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      top: 2.0,
-                      left: 8,
-                    ),
-                    child: AutoSizeText(
-                      'Stars',
-                      style: TextStyle(
-                        color: Colors.white,
-                      ),
-                      minFontSize: 12,
-                    ),
+                  Icon(
+                    Icons.stars_sharp,
+                    color: Colors.amberAccent,
                   )
                 ],
               ),
-              SizedBox(
-                width: 20,
-              ),
-              Container(
-                transform: Matrix4.translationValues(
-                  0,
-                  -8,
-                  0,
-                ),
-                child: Icon(
-                  Icons.card_giftcard,
-                  color: Colors.amber,
-                  size: 13,
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 10, left: 20),
+              alignment: Alignment.topLeft,
+              child: Text(
+                'Welcome back!',
+                style: TextStyle(
+                  color: HexColor("#175244"),
+                  fontSize: 35,
                 ),
               ),
-              Column(
-                children: [
-                  Container(
-                    transform: Matrix4.translationValues(
-                      -18,
-                      0,
-                      0,
-                    ),
-                    child: AutoSizeText(
-                      '1',
-                      style: TextStyle(
-                        color: Colors.white,
+            ),
+            Container(
+              padding: EdgeInsets.only(
+                top: 40,
+                bottom: 20,
+              ),
+              child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Container(
+                      child: Image.asset(
+                        'images/stars.png',
+                        width: 30,
                       ),
-                      minFontSize: 20,
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(
-                      top: 2.0,
-                      left: 8,
+                    Column(
+                      children: [
+                        AutoSizeText(
+                          '1/5',
+                          style: TextStyle(
+                            color: HexColor("#175244"),
+                          ),
+                          minFontSize: 15,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 5.0,
+                          ),
+                          child: AutoSizeText(
+                            'Stars',
+                            style: TextStyle(
+                              fontWeight: FontWeight.w600,
+                              color: HexColor("#175244"),
+                            ),
+                            minFontSize: 18,
+                          ),
+                        )
+                      ],
                     ),
-                    child: AutoSizeText(
-                      'Rewards',
-                      style: TextStyle(
-                        color: Colors.white,
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Container(
+                      child: Icon(
+                        Icons.card_giftcard,
+                        color: Colors.amber,
+                        size: 23,
                       ),
-                      minFontSize: 12,
                     ),
-                  )
-                ],
+                    Column(
+                      children: [
+                        Container(
+                          child: AutoSizeText(
+                            '1',
+                            style: TextStyle(
+                              color: HexColor("#175244"),
+                            ),
+                            minFontSize: 15,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(
+                            top: 5.0,
+                          ),
+                          child: AutoSizeText(
+                            'Rewards',
+                            style: TextStyle(
+                              color: HexColor("#175244"),
+                              fontWeight: FontWeight.w600,
+                            ),
+                            minFontSize: 18,
+                          ),
+                        )
+                      ],
+                    ),
+                  ]),
+            ),
+            SizedBox(height: 20,),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Image.asset(
+                  'images/leaf.png',
+                  width: 250,
+                ),
+                Container(
+                    margin: EdgeInsets.only(left: 8),
+                    child: Image.asset('images/trophy.png', width: 360)),
+              ],
+            ),
+            SizedBox(height: 20,),
+            Container(
+              margin: EdgeInsets.all(10),
+              child: AutoSizeText(
+                'You are 4 stars away from another reward',
+                style: TextStyle(
+                  color: HexColor("#175244"),
+                ),
+                minFontSize: 18,
               ),
-              Container(
-                width: MediaQuery.of(context).size.width * 0.4,
-                transform: Matrix4.translationValues(
-                  30,
-                  4,
-                  0,
-                ),
-                child: AutoSizeText(
-                  'You are 4 stars away from another reward',
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                  minFontSize: 15,
-                ),
-              ),]),)
-        ],
-      )),])
-    ));
+            ),
+            SizedBox(height: 20,),
+            ExpansionTile(
+              childrenPadding: const EdgeInsets.all(20.0),
+              iconColor: HexColor("#175244"),
+              collapsedIconColor:HexColor("#175244") ,
+              title: Text('Know more', style: TextStyle(color: HexColor("#175244")),),
+              children: [
+                Text(
+                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.')
+              ],
+            )
+          ],
+        )));
   }
 }
