f451a8b117bdf865eaf3c60304ce0d9ada0665d0
Sanish Aukhale <sanish.aukhale@srijan.net>
2022-12-06 16:42:21 +0530

    toast message for addtocar

--- lib/home/offers_data.dart
+++ lib/home/offers_data.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:like_button/like_button.dart';
 import 'dart:io' show Platform;
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:get/get.dart';
@@ -184,5 +185,15 @@ class _GetOffersState extends State<GetOffers> {
         },
       ),
     );
+  }
+
+  Future<bool> onLikeButtonTapped(bool isLiked) async {
+    /// send your request here
+    // final bool success= await sendRequest();
+
+    /// if failed, you can do nothing
+    // return success? !isLiked:isLiked;
+
+    return !isLiked;
   }
 }
