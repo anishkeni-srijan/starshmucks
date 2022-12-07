fec5e7e8807adc4c84bd7d9800a0493c93f4945b
Sanish Aukhale <sanish.aukhale@srijan.net>
2022-12-07 11:47:13 +0530

    updated toast message

--- lib/model/user_model.dart
+++ lib/model/user_model.dart
@@ -10,8 +10,8 @@ class UserModel {
   String tier;
   String image;
 
-  UserModel({
-      required this.tier,
+  UserModel(
+      {required this.tier,
       required this.name,
       required this.email,
       required this.phone,
@@ -19,23 +19,22 @@ class UserModel {
       required this.password,
       required this.tnc,
       required this.rewards,
-      required this.image
-      });
+      required this.image});
 
-  factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
-        tier:json["tier"],
-        name: json["name"],
-        email: json["email"],
-        phone: json["phone"],
-        dob: json["dob"],
-        password: json["password"],
-        tnc: json["tnc"],
-        rewards: json["rewards"], image: json["image"],
-        // id: json["id"],
-      );
+  // factory UserModel.fromJson(Map<String, dynamic> json) => UserModel(
+  //       tier:json["tier"],
+  //       name: json["name"],
+  //       email: json["email"],
+  //       phone: json["phone"],
+  //       dob: json["dob"],
+  //       password: json["password"],
+  //       tnc: json["tnc"],
+  //       rewards: json["rewards"], image: json["image"],
+  //       // id: json["id"],
+  //     );
 
   Map<String, dynamic> toMap() => {
-         "tier":tier,
+        "tier": tier,
         "name": name,
         "email": email,
         "phone": phone,
