a7743ba99ddc6f04f83b96d9520f0a221106e965
Sanish Aukhale <sanish.aukhale@srijan.net>
2022-12-08 15:35:11 +0530

    swatch color

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:google_fonts/google_fonts.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
+import 'package:hexcolor/hexcolor.dart';
 
 import '/forgotpassword/forgot_password.dart';
 import '/signup/bloc/signup_bloc.dart';
@@ -64,7 +65,7 @@ class MyApp extends StatelessWidget {
           title: 'Flutter Demo',
           theme: ThemeData(
             textTheme: GoogleFonts.ubuntuTextTheme(),
-            primarySwatch: Colors.blue,
+            primarySwatch: Colors.green,
           ),
           debugShowCheckedModeBanner: false,
           home: Splash(),
